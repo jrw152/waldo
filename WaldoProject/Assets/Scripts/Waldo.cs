dda6e3b31b377487174b044599c7c8c9bf8441b6
Andrew <awh32@case.edu>
2013-04-19 14:18:58 -0400

    Added movement to waldo

--- WaldoProject/Assets/Scripts/Waldo.cs
+++ WaldoProject/Assets/Scripts/Waldo.cs
@@ -10,29 +10,36 @@ public class Waldo : MonoBehaviour {
 	public bool disguised = false;
 	int swapTimer = 0;
 	public int maxSwapTimer = 10;
-	// Use this for initialization
+	
 	void Start () {
 
 	}
 	
+	//TODO: waldo is invisible if looking at guards
 	// Update is called once per frame
 	void Update () {
-			if(Input.GetKey(KeyCode.UpArrow)){
-				//move forward
+			/*basic movmement mechanics*/
+			if(Input.GetKey(KeyCode.W)){
+				transform.Translate(Vector3.forward*Time.deltaTime*moveSpeed);
 			}
-			if(Input.GetKey(KeyCode.DownArrow)){
-				//move backward
+			if(Input.GetKey(KeyCode.S)){
+				transform.Translate(Vector3.back*Time.deltaTime*moveSpeed);
 			}
-			if(Input.GetKey(KeyCode.RightArrow)){
-				//rotate camera
+			if(Input.GetKey(KeyCode.D)){
+				transform.Rotate(Vector3.right*Time.deltaTime);
 			}
-			if(Input.GetKey(KeyCode.LeftArrow)){
-				//rotate camera
+			if(Input.GetKey(KeyCode.A)){
+				transform.Rotate(Vector3.left*Time.deltaTime);
 			}
-			//TODO: check if mouse over waldites
+		
+			//teleport mechanics
+			//TODO: check if mouse over waldites?
+			// or maybe we will just limit it to once per run
 			if(Input.GetMouseButton(0)){
 				teleport();
 			}
+		
+			/* skin swap timer mechanics*/
 			if(disguised == true){
 				swapTimer++;
 			}
@@ -46,12 +53,13 @@ public class Waldo : MonoBehaviour {
 	void GetHat(){
 		haveHat = true;
 	}
-	//TODO: account for more than one group of waldites
+	//TODO: account for more than one group of waldites?
 	void teleport(){
 		if(haveHat == true){
 			Vector3 targetGroup = new Vector3(0,0,0);
 			targetGroup = waldites.transform.position;
 			transform.position = targetGroup;
+			//maybe play sound effect?
 		}
 	}
 	
