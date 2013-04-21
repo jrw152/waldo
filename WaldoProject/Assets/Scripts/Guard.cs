3abce4d020f1d1a1d105bb7281fb9fabbb412265
Andrew Hale <ajh110@case.edu>
2013-04-21 14:51:23 -0400

    Updated Guard Script
    I haven't been able to test it because I'm having trouble figuring out
    the scene. What do you guys think?

--- WaldoProject/Assets/Scripts/Guard.cs
+++ WaldoProject/Assets/Scripts/Guard.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class Guard : MonoBehaviour {
 	private bool hasSpotted;
 	public GameObject waldo;
-	public int xRange;
-	public int yRange;
-	public int zRange;
-
+	public float xRange = 10.0f;
+	public float yRange = 10.0f;
+	public float zRange = 10.0f;
+	public float xRate = 1.0f;
+	public float yRate = 1.0f;
+	public float zRate = 1.0f;
+	private Vector3 mover = Vector3.zero;
 	// Use this for initialization
 	void Start () {
 		hasSpotted = false;
@@ -15,8 +18,16 @@ public class Guard : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		mover = this.transform.position;
+		CharacterController controller = GetComponent<CharacterController>();
 		if(waldo.transform.position.x <= xRange || waldo.transform.position.y <= yRange || waldo.transform.position.z <= zRange){
 			hasSpotted = true;
+			if(waldo.transform.position.x <= xRange){mover.x = mover.x + xRate;}
+			if(waldo.transform.position.y <= yRange){mover.y = mover.y +yRate;}
+			if(waldo.transform.position.z <= zRange){mover.z = mover.z + zRate;}
+			controller.Move (mover*Time.deltaTime);
+			
+			
 		}
 		if(hasSpotted == true){
 			BroadcastMessage("SummonGargoyle", null);
