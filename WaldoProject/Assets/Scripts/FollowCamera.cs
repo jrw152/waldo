bc03873312cab69f0dfe47781902d40a384ba0bf
Geoff Miller <gdm37@case.edu>
2013-04-25 05:56:22 -0400

    Let's see what happens, eh?

--- WaldoProject/Assets/Scripts/FollowCamera.cs
+++ WaldoProject/Assets/Scripts/FollowCamera.cs
@@ -5,16 +5,21 @@ using System.Collections;
 public class FollowCamera : MonoBehaviour {
     public GameObject target;
     Vector3 offset;
+	Vector3 yAdjust = new Vector3(0,5,0);
     void Start() {
+		target = GameObject.FindGameObjectWithTag("Waldo");
         offset = target.transform.position - transform.position;
     }
 	
 	// Update is called once per frame
 	void LateUpdate() {
 	    float desiredAngle = target.transform.eulerAngles.y;
-	    Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-		transform.position = target.transform.position - (rotation * offset);		
+	    Quaternion rotation = Quaternion.Euler(-15, desiredAngle - 15, 0);
+		transform.position = target.transform.position - (rotation * offset);
+		
+		
 		transform.LookAt(target.transform);
+		transform.position = transform.position + yAdjust;
 	}
 			
 }
